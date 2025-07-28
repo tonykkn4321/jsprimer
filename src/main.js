aad7fd833fb0d250a7a4cf3aeef0e91e256f045b
Tony Kwong <tonykkn4321@gmail.com>
2025-07-28 12:28:48 +0800

    4th commit

--- src/main.js
+++ src/main.js
@@ -13,6 +13,13 @@ import * as ops from "./maths/operations";
 //  Listing 4-48. Importing an Entire Module in the main.js:
 import * as math from "./maths";
 
+// Listing 4-51. Performing Background Work:
+import { asyncAdd } from "./maths";
+
+// Listing 4-52. Using a Promise in the index.js:
+import { asyncAdd_promise } from "./maths";
+
+
 let values = [10, 20, 30, 40, 50];
 
 // Listing 4-42. Using a Simple JavaScript Module:
@@ -35,6 +42,24 @@ console.log(`Multiply: ${math.multiply(values)}`);
 console.log(`Subtract: ${math.subtract(1000, values)}`);
 console.log(`Mean: ${math.mean(values)}`);
 
+// Listing 4-51. Performing Background Work:
+let total_async = asyncAdd(values);
+console.log(`Main ASYNC Total: ${total_async}`);
 
+// Listing 4-52. Using a Promise in the index.js:
+let total_async_promise = asyncAdd_promise(values);
+console.log(`Main ASYNC Promise Total: ${total_async_promise}`);
+
+// Listing 4-53. Observing a Promise in the main.js:
+asyncAdd_promise(values).then(total_async_promise => console.log(`Main ASYNC Promise Total: ${total_async_promise}`));
+
+
+// Listing 4-54. Using async and await in the main.js:
+async function doTask() {
+    let total = await asyncAdd_promise(values);
+    console.log(`Main Simplied Async Total: ${total}`);
+}
+
+doTask();
 
 createApp(App).mount('#app')
--- src/maths/index.js
+++ src/maths/index.js
@@ -9,3 +9,24 @@ export function mean(values) {
 
 export { addition };
 export * from "./operations";
+
+//  Listing 4-50. Adding a Function in the index.js:
+export function asyncAdd(values) {
+    setTimeout(() => {
+    let total = addition(values);
+    console.log(`Async Total: ${total}`);
+return total;
+}, 500);
+}
+
+
+// Listing 4-52. Using a Promise in the index.js :
+export function asyncAdd_promise(values) {
+    return new Promise((callback) => {
+        setTimeout(() => {
+        let total = addition(values);
+        console.log(`Async_promise Total: ${total}`);
+        callback(total);
+        }, 500);
+    });
+}
